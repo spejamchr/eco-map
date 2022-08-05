c7901a47172e65168c3ea4074997ae51ce234394
Spencer Christiansen <jc.spencer92@gmail.com>
2022-08-05 17:23:08 -0600

    old stuff

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+extern crate noise;
+
+use crate::noise::utils::NoiseMapBuilder;
+use crate::noise::MultiFractal;
+use noise::utils::PlaneMapBuilder;
+use noise::HybridMulti;
+
 fn main() {
-    println!("Hello, world!");
+    let n = HybridMulti::default().set_octaves(8).set_persistence(0.5);
+
+    PlaneMapBuilder::new(&n)
+        .set_size(1000, 1000)
+        .set_x_bounds(-5.0, 5.0)
+        .set_y_bounds(-5.0, 5.0)
+        .build()
+        .write_to_file("perlin.png");
 }
